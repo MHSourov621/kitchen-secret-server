562fea4c27a853197f5851555324a303e197b866
MHSourov621 <mhsourov621@gmail.com>
2023-05-04 01:00:16 +0600

    added json data

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express')
 const app = express()
 const port = 5000;
 
-
+const allServices = require("./data.json");
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
-  
-  app.listen(port, () => {
+})
+
+app.get('/allData', (req, res) => {
+    res.send(allServices)
+})
+
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
