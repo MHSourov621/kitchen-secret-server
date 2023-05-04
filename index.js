c5e9718d229fb83585fe9af7464c415f81165bf1
MHSourov621 <mhsourov621@gmail.com>
2023-05-04 12:47:32 +0600

    vercel.json added

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ app.get('/allData/:id', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port`)
 })
